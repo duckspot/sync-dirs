8de08520b1e057f3baa3a2ac3876a9d27db76f0c
Peter Dobson <peter@duckspot.com>
2014-03-11 13:06:49 -0700

    Create Maven Command Line App.

---
+++ src/main/java/com/duckspot/sync/App.java
@@ -0,0 +1,13 @@
+package com.duckspot.sync;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
